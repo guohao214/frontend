372c7da1c4f013026678167c9b3dfee593aee1be
郭浩 <guohao214@gmail.com>
2017-07-13 15:26:13 +0800

    1

--- worker/worker.js
+++ worker/worker.js
@@ -1,13 +1,18 @@
 self.onmessage = function( event ) {
+	console.log(self)
 	var num = event.data
 
-	var t = setInterval( function() {
-		self.postMessage( --num )
+  self.postMessage(num)
 
-		if ( num <= 0 ) {
-			clearInterval(t)
-			self.close()
-		}
 
-	}, 1000)
+  //
+	// var t = setInterval( function() {
+	// 	self.postMessage( --num )
+  //
+	// 	if ( num <= 0 ) {
+	// 		clearInterval(t)
+	// 		self.close()
+	// 	}
+  //
+	// }, 1000)
 }
